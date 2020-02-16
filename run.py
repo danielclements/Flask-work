badcdeba100207e719db1c9ceb1c4acc8a7c47de
Daniel Clements <daniel.clements14@gmail.com>
2020-02-16 18:24:42 +0000

    heroku update mass push

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
+app.secret_key='this_is_a_secret'
 
 
 @app.route('/')
@@ -41,8 +42,12 @@ def about_member(member_name):
     
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message".format(
+            request.form["name"]
+        ))
     return render_template("contact.html", page_title="Contact")
 
 
